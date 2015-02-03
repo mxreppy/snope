21c4b50b1b7308f307051ad8f1b984bc30038170
Mikey Reppy <mxreppy@liquidhub.com>
2015-02-03 09:09:41 -0500

    more debugging

--- server.js
+++ server.js
@@ -24,14 +24,20 @@ soap.createClient(wsdl_url, function(err, client) {
 
         console.log('now what? -- calling GetInfoByZIP ');
 
-        client.GetInfoByZIP( args, function( zip_err, zip_result ) {
+        client.GetInfoByZIP( args, function( zip_err, result, raw, soapHeader ) {
             if( zip_err ) {
+                console.error( 'error!');
+                
                 console.error(zip_err);
 
                 throw zip_err;
             } else {
                 console.log("got zip result");
-                console.log( util.inspect( zip_result, {maxDepth:4} ) );
+                console.log( util.inspect( result, {maxDepth:4} ) );
+                console.log("got soap header");
+                console.log( util.inspect( soapHeader, {maxDepth:4} ) );
+                console.log("raw");
+                console.log( util.inspect( raw, {maxDepth:4} ) );
             }
         } );
     }
